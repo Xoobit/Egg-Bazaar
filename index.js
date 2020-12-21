7f22e23cc66e8a46eafdb7a6bb2c2f075ea88333
abcdra <abdraloguzov@mail.ru>
2020-12-22 01:38:44 +0500

    working version

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const path = require("path")
+
+const file2table = require("./parser")
+
 const app = express()
 const port = 3000
+const PAGES_FOLFER = path.join(__dirname, "pages")
+
+
+app.use('/static', express.static('static'))
+app.set('view engine', 'hbs');
+app.engine('html', require('hbs').__express);
 
 app.get('/', (req, res) => {
-  res.send('Продаю яйца(анзул пидор)')
+  const table = file2table().trim()
+  return res.render(PAGES_FOLFER+"/index.html", {table})
 })
 
 app.listen(port, () => {
---
+++ parser.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const path = require("path")
+const PATH_LOG = path.join(__dirname, "dism.log")
+
+
+const template = {
+    "date" : [0, 19], 
+    "type" : [21, 43],
+    "source" : [43, 54],
+    "content":[54]
+}
+
+
+function file2table() {	
+    let fileContent = fs.readFileSync(PATH_LOG, "utf8");
+    return txt2log(fileContent)
+}
+
+
+//Преобразуем строку текста в таблицу(массив объект-строчек)
+function txt2log(txt) {
+    let logTable = '<table id="log"><tbody>'
+    //разделяем все строчки в массив по \n
+    const rows = txt.split("\n")
+    rows.forEach(row => {
+        let hRow = separate(row)
+        logTable += hRow
+    })
+    return logTable + "</tbody></table>"
+} 
+
+function separate(row) {
+    //Если строка пустая или пробельная, то возвращаем пустую строку
+    if (row.trim() === "") {
+        return ""
+    }
+    let hRow = "<tr>"
+    t = template
+    //Если первая строчка начинается не с цифры то это строка исключение
+    if(isNaN(row[0]) || row.length < 54) {
+        return '<tr><td class="Date"> </td><td class="Type"> </td><td class="Source"> </td>' +
+        '<td class="Content">'+ row + '</td></tr>'
+    }
+    hRow += '<td class="Date">'+ row.substring(t["date"][0], t["date"][1]).trim() + '</td>'
+    hRow += '<td class="Type">'+ row.substring(t["type"][0], t["type"][1]).trim() + '</td>'
+    hRow += '<td class="Source">'+ row.substring(t["source"][0], t["source"][1]).trim() + '</td>'
+    hRow += '<td class="Contens">'+ row.substr(t["content"][0]).trim() + '</td>'
+    return hRow + "</tr>"
+}
+
+module.exports = file2table
