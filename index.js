ed4a3bae711a6d196035c3b861fdf49bcf951482
abcdra <abdraloguzov@mail.ru>
2020-12-24 20:33:37 +0500

    some comment

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
+//Импортируем модуль с фреймворком
 const express = require('express')
+//Импортируем модуль path для работы с путями
 const path = require("path")
 
+//Импортируем функцию из файла parser, подробней смотреть в самом файле
 const file2table = require("./parser")
 
+//Инициализируем приложение
 const app = express()
+//Устанавливаем порт
 const port = 3000
+//Указываем папку где хранятся html странички
 const PAGES_FOLFER = path.join(__dirname, "pages")
 
-
+//Указываем папку со статическими файлами
 app.use('/static', express.static('static'))
+//Используем шаблонизатор hbs
+//Шаблонизатор нужен для передачи данных из приложения на страничку
 app.set('view engine', 'hbs');
+//Указываем расширения файла с шаблоном, в нашем случае html
 app.engine('html', require('hbs').__express);
 
+
+//Назначаем функцию которая будет выполнятся когда кто-нибудь обращается к 
+//главной странице сайта /
 app.get('/', (req, res) => {
+  //Используем функцию для получения html таблицы из файла, обрезаем пробелы на всякий случай
   const table = file2table().trim()
+  //Берем шаблон index.html из папки и передаем туда таблицу
   return res.render(PAGES_FOLFER+"/index.html", {table})
 })
 
+//Запуск приложения 
 app.listen(port, () => {
   console.log(`Сервер запущен http://localhost:${port}`)
 })
--- parser.js
+++ parser.js
@@ -1,8 +1,11 @@
+//Импортируем модуль fs для работы с файловой системой
 const fs = require("fs");
 const path = require("path")
+//Указываем путь до логов
 const PATH_LOG = path.join(__dirname, "dism.log")
 
-
+//В данном объекты храним индексы для обрезания строки лога
+//Например дата хранится между 0 и 19 индексом, тип между 21 и 43
 const template = {
     "date" : [0, 19], 
     "type" : [21, 43],
@@ -11,8 +14,11 @@ const template = {
 }
 
 
+//Главная функция модуля
 function file2table() {	
+    //Получаем контент файла
     let fileContent = fs.readFileSync(PATH_LOG, "utf8");
+    //Преобразум контент через функцию txt2log
     return txt2log(fileContent)
 }
 
@@ -23,30 +29,38 @@ function txt2log(txt) {
     '<tr><th>Date</th><th>Type</th><th>Source</th><th>Content</th></tr>'
     //разделяем все строчки в массив по \n
     const rows = txt.split("\n")
+    //Бегаем по каждой строке используем функцию separate для преоброзования и записываем все в переменную logTable
     rows.forEach(row => {
         let hRow = separate(row)
         logTable += hRow
     })
+    //Закрываем тэги и возвращаем logTable
     return logTable + "</tbody></table>"
 } 
+
 
 function separate(row) {
     //Если строка пустая или пробельная, то возвращаем пустую строку
     if (row.trim() === "") {
         return ""
     }
+    //Начинаем с тега строки
     let hRow = "<tr>"
-    t = template
+    //Сокращаем имя template до t
+    const t = template
     //Если первая строчка начинается не с цифры то это строка исключение
     if(isNaN(row[0]) || row.length < 54) {
         return '<tr><td class="Date"> </td><td class="Type"> </td><td class="Source"> </td>' +
         '<td class="Content">'+ row + '</td></tr>'
     }
+    //Запихиваем обрезанные по шаблону строки в тэги ячеек
     hRow += '<td class="Date">'+ row.substring(t["date"][0], t["date"][1]).trim() + '</td>'
     hRow += '<td class="Type">'+ row.substring(t["type"][0], t["type"][1]).trim() + '</td>'
     hRow += '<td class="Source">'+ row.substring(t["source"][0], t["source"][1]).trim() + '</td>'
     hRow += '<td class="Contens">'+ row.substr(t["content"][0]).trim() + '</td>'
+    //Закрываем тэг и возвращаем html строчку
     return hRow + "</tr>"
 }
 
+//Экспортируем(делаем доступным) функцию file2table другим модулям
 module.exports = file2table
